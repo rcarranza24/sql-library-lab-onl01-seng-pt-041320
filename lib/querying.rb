3ffbce54569935f2e5540e8ea3c923778900c756
Rosa Carranza <rcarranza1024@yahoo.com>
2020-06-14 20:16:24 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -48,5 +48,10 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "SELECT characters.name, (COUNT(character_books.book_id)) as total_books
+ FROM character_books
+ INNER JOIN characters
+ ON character_books.character_id = characters.id
+ GROUP BY characters.name
+ ORDER BY total_books DESC "
 end
