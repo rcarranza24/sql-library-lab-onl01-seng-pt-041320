2f7e17e9185bc738cc01bf2ff960fa0ed58260bf
Rosa Carranza <rcarranza1024@yahoo.com>
2020-06-14 20:16:07 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -36,7 +36,15 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT series.title
+   FROM series
+   JOIN characters
+   ON series.author_id = characters.author_id
+   GROUP BY series.title
+   HAVING characters.species = 'human'
+   ORDER BY characters.species DESC
+   LIMIT 1;
+   "
 end
 
 def select_character_names_and_number_of_books_they_are_in
